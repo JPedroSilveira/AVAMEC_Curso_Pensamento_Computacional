17aa2e6b7b579c96aac1f82a72077ed877b0a454
João Pedro Silveira <jpedross1999@gmail.com>
2019-03-30 08:06:17 -0300

    Refactor(QuestionTip): Mudança para cores mais suaves

--- src/constants/color.js
+++ src/constants/color.js
@@ -1,6 +1,6 @@
 export default {
-    HINT_RIGHT_BG: 'green',
-    HINT_WRONG_BG: 'rgb(255, 94, 36)',
+    HINT_RIGHT_BG: 'rgb(169, 249, 169)',
+    HINT_WRONG_BG: 'rgb(255, 157, 124)',
     PROBLEM_BOX_BG: 'rgb(251, 229, 205)',
     GRAY_BOX: 'rgb(207, 226, 243)'
 }
