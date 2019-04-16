f47f9519ff4874a39f3c8460722170e2142d0568
João Pedro Silveira <jpedross1999@gmail.com>
2019-04-16 17:07:44 -0300

    Feat(MiniGamePage1): Adição do mini game na página 1 da Unidade 2

---
+++ src/components/scenes/introducao_ao_pensamento_computacional/aplicacao_interativa/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MiniGameConceitosInterativos from '../../../../game-iframes/conceitos_interativos'
+
+class AplicacaoInterativa extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Mini-jogo: Conceitos Interativos</h2>
+                <MiniGameConceitosInterativos />
+            </div>
+        )
+    }
+}
+export default AplicacaoInterativa
